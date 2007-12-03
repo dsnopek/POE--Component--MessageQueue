ca69756c24bc69b842f0417eb8193edbe8576883
David Snopek <dsnopek@gmail.com>
2007-12-03 08:09:04 -0600

    Auto upgrade old databases that use the defaults (Storage::Complex).

--- schema/sqlite.sql
+++ schema/sqlite.sql
@@ -5,7 +5,9 @@ CREATE TABLE messages
 	destination varchar(255) not null,
 	persistent  char(1) default 'Y' not null,
 	in_use_by   int,
-	body        text
+	body        text,
+	timestamp   int,
+	size        int
 );
 
 -- Improves performance some bit:
