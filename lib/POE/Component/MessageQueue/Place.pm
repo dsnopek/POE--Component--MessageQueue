e1e1f1a37a0a9a63fc56c146fc6109e9d5744715
Paul Driver <frodwith@gmail.com>
2008-02-11 16:36:08 -0600

    Forgot this file in last commit (parent role for Topic and Queue)

---
+++ lib/POE/Component/MessageQueue/Place.pm
@@ -0,0 +1,41 @@
+#
+# Copyright 2007 Paul Driver <frodwith@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+package POE::Component::MessageQueue::Place;
+use Moose::Role;
+
+has 'parent' => (
+	is       => 'ro',
+	required => 1,
+	handles  => [qw(log notify storage dispatch_message)],
+);
+
+has 'subscriptions' => (
+	is      => 'ro',
+	isa     => 'HashRef',
+	default => sub { {} },
+);
+
+has 'name' => (
+	is       => 'ro',
+	required => 1,
+);
+
+requires qw(destination send is_persistent);
+
+1;
+
