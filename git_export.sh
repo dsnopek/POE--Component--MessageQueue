98a29d4d49dc0e1e332b32a12fee316510b133ae
David Snopek <dsnopek@gmail.com>
2010-06-05 11:11:18 +0200

    Some fixes to my git_export.sh script!

--- git_export.sh
+++ git_export.sh
@@ -3,6 +3,6 @@
 EXPFILE=/tmp/pocomq-fastexport
 
 bzr fast-export --plain . $EXPFILE && \
-(cd ../git.mainline && git fast-import < $EXPFILE && git commit -m "Importing from bzr")
+(cd ../git.mainline && git fast-import < $EXPFILE && git push)
 rm -f $EXPFILE
 
